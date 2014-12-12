0a171ab3e493fcb222a7e92177761ae4a9a91451
Prashant Bhangre <prashantgbhangre@gmail.com>
2014-12-12 15:16:58 +0530

    First Draft

---
+++ customized-navigation-drawer/gen/com/prashantb/customized_navigation_drawer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.prashantb.customized_navigation_drawer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
